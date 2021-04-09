7e7bd56fc267a4c5295fd72785b8a911aaa0f04d
Demian Daniel <daniel.demian@student.umb.sk>
2021-04-09 10:58:17 +0200

    Vytvorenie layoutu Hlavného okna (Plátno + bočný panel)

---
+++ src/main/java/GUI/Platno.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Platno extends JPanel {
+
+    private int WIDTH = 600;
+    private int HEIGHT = 600;
+
+    public int getWidth(){
+        return WIDTH;
+    }
+
+    public int getHeight(){
+        return HEIGHT;
+    }
+}

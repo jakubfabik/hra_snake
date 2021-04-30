def7e6ba1b00b6f2dbc3df27dfe1b42be20f1501
Demian Daniel <daniel.demian@student.umb.sk>
2021-04-30 10:35:56 +0200

    Vytvorenie UvodnejObrazovky (Hráč si vyberie meno)

--- src/main/java/GUI/UvodnaObrazovka.java
+++ src/main/java/GUI/UvodnaObrazovka.java
@@ -5,14 +5,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import GUI.Okno;
-
-
 public class UvodnaObrazovka extends JFrame{
 
     JFrame frame = new JFrame("Snake");
     static JSpinner s1, s2, s3;
-
+    String meno;
     public UvodnaObrazovka(){
         OvladaciePrvky();
         OknoFcia();
@@ -32,15 +29,15 @@ public class UvodnaObrazovka extends JFrame{
         panel.setLayout(new BorderLayout());
 
         JPanel north = new JPanel();
-        north.setBackground(Color.DARK_GRAY);
+        north.setBackground(Color.ORANGE);
         JLabel nadpis = new JLabel();
         nadpis.setText("Vyber si meno:");
         nadpis.setFont(new Font("Serif", Font.BOLD, 20));
-        nadpis.setForeground(Color.WHITE);
+        nadpis.setForeground(Color.BLACK);
         north.add(nadpis);
 
         JPanel south = new JPanel();
-        south.setBackground(Color.DARK_GRAY);
+        south.setBackground(Color.GREEN);
         JButton tlacidlo = new JButton("Potvrdit");
         tlacidlo.setBackground(Color.WHITE);
         tlacidlo.setForeground(Color.BLACK);
@@ -49,37 +46,40 @@ public class UvodnaObrazovka extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
-                Okno okno = new Okno();
-                String meno = s1.getValue() + " " + s2.getValue() + " " + s3.getValue();
+                meno = s1.getValue() + "" + s2.getValue() + "" + s3.getValue();
+                System.out.println(meno);
+                Okno okno = new Okno(meno);
             }
         });
 
         JPanel center = new JPanel();
-        center.setBackground(Color.DARK_GRAY);
         center.setLayout(new GridLayout(0,3));
         String abeceda[] = {"A", "B", "C", "D", "E","F", "G",
                 "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
                 "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
         };
         s1 = new JSpinner(new SpinnerListModel(abeceda));
-        s1.setFont(new Font("Serif", Font.BOLD, 400));
+        s1.setFont(new Font("Monospaced", Font.PLAIN, 400));
         ((JSpinner.DefaultEditor) s1.getEditor()).getTextField().setEditable(false);
-        ((JSpinner.DefaultEditor) s1.getEditor()).getTextField().setBackground(Color.GRAY);
-        ((JSpinner.DefaultEditor) s1.getEditor()).getTextField().setForeground(Color.WHITE);
+        ((JSpinner.DefaultEditor) s1.getEditor()).getTextField().setBackground(Color.YELLOW);
+        ((JSpinner.DefaultEditor) s1.getEditor()).getTextField().setForeground(Color.BLACK);
+        ((JSpinner.DefaultEditor) s1.getEditor()).getTextField().setHorizontalAlignment(JTextField.CENTER);
         center.add(s1);
 
         s2 = new JSpinner(new SpinnerListModel(abeceda));
-        s2.setFont(new Font("Serif", Font.BOLD, 400));
+        s2.setFont(new Font("Monospaced", Font.PLAIN, 400));
         ((JSpinner.DefaultEditor) s2.getEditor()).getTextField().setEditable(false);
-        ((JSpinner.DefaultEditor) s2.getEditor()).getTextField().setBackground(Color.GRAY);
-        ((JSpinner.DefaultEditor) s2.getEditor()).getTextField().setForeground(Color.WHITE);
+        ((JSpinner.DefaultEditor) s2.getEditor()).getTextField().setBackground(Color.YELLOW);
+        ((JSpinner.DefaultEditor) s2.getEditor()).getTextField().setForeground(Color.BLACK);
+        ((JSpinner.DefaultEditor) s2.getEditor()).getTextField().setHorizontalAlignment(JTextField.CENTER);
         center.add(s2, BorderLayout.CENTER);
 
         s3 = new JSpinner(new SpinnerListModel(abeceda));
-        s3.setFont(new Font("Serif", Font.BOLD, 400));
+        s3.setFont(new Font("Monospaced", Font.PLAIN, 400));
         ((JSpinner.DefaultEditor) s3.getEditor()).getTextField().setEditable(false);
-        ((JSpinner.DefaultEditor) s3.getEditor()).getTextField().setBackground(Color.GRAY);
-        ((JSpinner.DefaultEditor) s3.getEditor()).getTextField().setForeground(Color.WHITE);
+        ((JSpinner.DefaultEditor) s3.getEditor()).getTextField().setBackground(Color.YELLOW);
+        ((JSpinner.DefaultEditor) s3.getEditor()).getTextField().setForeground(Color.BLACK);
+        ((JSpinner.DefaultEditor) s3.getEditor()).getTextField().setHorizontalAlignment(JTextField.CENTER);
         center.add(s3, BorderLayout.WEST);
 
         panel.add(north, BorderLayout.NORTH);
